a9ddcbe02604d12918b90a87b0206b85240dd137
Gabriel Riveros <gseclen99@gmail.com>
2023-04-19 21:45:45 -0500

    Gabriel Riveros
    Mostrar data : Arma

--- ConsoleApp1/ConsoleApp1/Arma.cs
+++ ConsoleApp1/ConsoleApp1/Arma.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Arma : Item
+    class Arma : Item, MostrarDatos
     {
         int daño;
 
@@ -17,6 +17,12 @@ namespace ConsoleApp1
             this.daño = daño;
             this.tipos = ItemTipos.Arma;
         }
+
+        public string Mostrar()
+        {
+            return $"{nombre}, {precio}, {daño}";
+        }
+
         public override float Usar()
         {
             return daño;
