b7426f9771cc8d8f4cd1a52580051a31c4fd2668
iDinoDave <97319831+iDinoDave@users.noreply.github.com>
2023-04-19 21:05:04 -0500

    David Achachagua
    Clase Vendedor

---
+++ ConsoleApp1/ConsoleApp1/Vendedor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Vendedor : NPC
+    {
+
+        float dinero;
+        List<Item> items;
+        public override void Morir()
+        {
+            vida = 0;
+        }
+
+        public Vendedor(string nombre, int vida, List <Item> items) {
+        
+            this.nombre= nombre;
+            this.vida= vida;
+            this.items = items;
+            this.tipo = Tipos.Vendedores;
+
+        
+        }
+
+    }
+}
