7b8fd4be3a563f11cdee2dd73e5211474695695b
rodrigocormel <go.cormel@hotmail.com>
2023-04-19 20:38:52 -0500

    Rodrigo Cordova
    Jugador NPC

---
+++ ConsoleApp1/ConsoleApp1/Jugador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Jugador
+    {
+        string nombre;
+        int experiencia;
+        float dinero;
+        int nivel;
+
+        public Jugador(string nombre, int experiencia, float dinero, int nivel) 
+        {
+            this.nombre = nombre;
+            this.experiencia = experiencia;
+            this.dinero = dinero;
+            this.nivel = nivel;
+        }
+        public int GanarExp(int experiencia)
+        {
+            return this.experiencia += experiencia;
+        }
+        public float RestarDinero (float dinero)
+        {
+            return this.dinero -= dinero;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/NPC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum Tipos
+    {
+        Enemigos,
+        Vendedores,
+        Conversadores
+    }
+    abstract class NPC
+    {
+        string nombre;
+        int vida;
+        public Tipos tipo;
+
+        public abstract void Morir();
+    }
+
+}
